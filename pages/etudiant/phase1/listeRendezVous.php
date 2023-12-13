477ac1bba869bf6a185489e67697e0c773e7ccd3
Benicol <benjamin.nicol@iut-rodez.fr>
2023-12-13 14:31:38 +0100

    en cours

--- pages/etudiant/phase1/listeRendezVous.php
+++ pages/etudiant/phase1/listeRendezVous.php
@@ -4,34 +4,20 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../bootstrap-4.6.2-dist/css/bootstrap.css">
-        <link rel="stylesheet" href="../fontawesome-free-6.2.1-web/css/all.css">
-        <title>pas dev</title>
+        <link  rel="stylesheet" href="tmp.css">
+        <link rel="stylesheet" href="../../../lib/bootstrap-5.3.2-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="../../../lib/fontawesome-free-6.5.1-web/css/all.css">
+        <title>Eureka - Liste des shouaits</title>
     </head>
     <body>
-        <?php
-            
-            // Récupérer le nom de la page
-            $currentPage = basename(__FILE__);
-
-
-            // Afficher le nom de la page
-            echo "<h1>Page: $currentPage</h1>";
-
-            echo "<p>Cette page n'est pas développer.</p>";
-
-
-            // Afficher les variables de session
-            echo "<h2>Variables de session :</h2>";
-            var_dump($_SESSION);
-
-            // Afficher les variables POST
-            echo "<h2>Variables POST :</h2>";
-            var_dump($_POST);
-
-            // Afficher les variables GET
-            echo "<h2>Variables GET :</h2>";
-            var_dump($_GET);
-        ?>
+        <div class="row">
+            <div class="col-10 align-items-center justify-content-center box">
+                <Button class="btn"><img src="../../../ressources/logo.png"/><span class="h2 texte">Eureka</spawn></Button>
+            </div>
+            <div class="col-2 d-sm-none">
+                <form action="../../deconnexion.php" method="post">
+                    <button type="submit" class="btn"><img src="../../../ressources/deconnexion.png"/></button>
+            </div>
+        </div>
     </body>
 </html>
