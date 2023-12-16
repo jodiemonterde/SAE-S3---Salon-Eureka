e4902a6a215a3ae4cd1720b285c89891a2842845
NoahMiquel <noah.miquel@iut-rodez.fr>
2023-12-16 18:05:58 +0100

    php de connexion

--- fonctions/baseDeDonnees.php
+++ fonctions/baseDeDonnees.php
@@ -1,2 +1,62 @@
 <?php
+    function connecteBD() {
+        // Database configuration
+        $host = 'mysql-sae-nmms.alwaysdata.net';
+        $dbName = 'sae-nmms_eureka';
+        $username = 'sae-nmms';
+        $password = 'NicolMonterdeMiquelSchardt';
+    
+        try {
+            // Create a new PDO instance
+            $pdo = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+            
+            // Set PDO error mode to exception
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            // You can now use the $pdo object to perform database operations
+            
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+        return $pdo;
+    }
+
+    function verifUtilisateur($pdo, $motDepasse, $identifiant){
+        try{ 
+			$connecte=false;
+			$maRequete = $connexion->prepare("SELECT user_id, username, password from User where username = :leLogin and password = :lePWD");
+			$maRequete->bindParam(':leLogin', $identifient);
+			$maRequete->bindParam(':lePWD', $motDepasse);
+			if ($maRequete->execute()) {
+				$maRequete->setFetchMode(PDO::FETCH_OBJ);
+				while ($ligne=$maRequete->fetch()) {				
+					$connecte=true;
+				}
+			}
+			return $connecte;
+		}
+		catch ( Exception $e ) {
+			echo "<h1>Erreur de connexion à la base de données ! </h1>";
+			return false;
+		} 
+    }
+
+    function infoUtilisateur($pdo, $motDepasse, $identifiant){
+        try{ 
+			$maRequete = $connexion->prepare("SELECT user_id, responsability from User where username = :leLogin and password = :lePWD");
+			$maRequete->bindParam(':leLogin', $identifient);
+			$maRequete->bindParam(':lePWD', $motDepasse);
+			if ($maRequete->execute()) {
+				$maRequete->setFetchMode(PDO::FETCH_OBJ);
+				while ($ligne=$maRequete->fetch()) {				
+					$_SESSION['idUtilisateur'] = $ligne->user_id;
+                    $_SESSION['typeUtilisateur'] = $ligne->responsability;
+				}
+			}
+		}
+		catch ( Exception $e ) {
+			echo "<h1>Erreur de connexion à la base de données ! </h1>";
+			return false;
+		} 
+    }
 ?>
--- pages/connexion.php
+++ pages/connexion.php
@@ -1,4 +1,23 @@
-<?php session_start(); ?>
+<?php 
+    session_start(); 
+    require("../fonctions/baseDedonnees.php");
+    $pdo = connecteBD();
+    $tentative = false;
+   
+
+    if(isset($_POST("motDePasse")) && isset($_POST("identifiant"))){
+        $_SESSION['connexion'] = verifUtilisateur($pdo, $_POST("motDePasse"), $_POST("identifiant"));
+        if($_SESSION['connexion'] == false){
+            $tentative = true;
+        }
+    }
+
+    if($_SESSION['connexion']==true){
+        infoUtilisateur($pdo, $_POST("motDePasse"), $_POST("identifiant"));
+        header("../index.php");
+    }
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -15,32 +34,40 @@
             <div class="row mx-1">
                 <div class="col-md-4 "></div>
                 <div class="col-12 col-md-4 centrer">
-                    <div class="row">
-                        <div class="col-12">
-                            <h1 class="text-center"> CONNEXION </h1>
+                    <form action="connexion.php" method="post">
+                        <?php 
+                            if($tentative){
+                                echo  '<h1 class="erreur"> identifiant ou mot de passe invalide </h1>';
+                            }
+                        ?>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <h1 class="text-center"> CONNEXION </h1>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <label for="identifiant"> Votre identifiant : </label>
-                            <input type="text" name="identifiant" value="" placeholder=" &#xf007 Saisir votre identifiant" class="form-control zoneText"/>
+                        <div class="row">
+                            <div class="col-12">
+                                <label for="identifiant"> Votre identifiant : </label>
+                                <input type="text" name="identifiant" value="" placeholder=" &#xf007 Saisir votre identifiant" class="form-control zoneText"/>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <label for="motDePasse"> Votre mot de passe : </label>
-                            <input type="password" name="motDePasse" value="" placeholder=" &#xf023 Saisir mot de passe" class="form-control zoneText"/>
-                            <label for="motDePasse" class="w-100 d-flex justify-content-end souligner"> Mot de passe oublié ? </label>
+                        <div class="row">
+                            <div class="col-12">
+                                <label for="motDePasse"> Votre mot de passe : </label>
+                                <input type="password" name="motDePasse" value="" placeholder=" &#xf023 Saisir mot de passe" class="form-control zoneText"/>
+                                <label for="motDePasse" class="w-100 d-flex justify-content-end souligner"> Mot de passe oublié ? </label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6 text-center">
-                            <button> Retour </button>
+                        <div class="row">
+                            <div class="col-6 text-center">
+                                <button> Retour </button>
+                            </div>
+                            <div class="col-6 text-center">
+                                <button> Se connecter </button>
+                            </div>
                         </div>
-                        <div class="col-6 text-center">
-                            <button> Se connecter </button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
                 <div class="col-md-4 "></div>
             </div>
