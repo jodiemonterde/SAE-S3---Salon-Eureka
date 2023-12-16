1476fa7a66dea4ddbf3ba2bcb9fd117839cde1bc
Benicol <benjamin.nicol@iut-rodez.fr>
2023-12-16 12:41:28 +0100

    en cours, page emploiDuTemp (etudiant)

--- fonctions/baseDeDonnees.php
+++ fonctions/baseDeDonnees.php
@@ -1,2 +1,55 @@
 <?php
+function connecteBD() {
+    // Database configuration
+    $host = 'mysql-sae-nmms.alwaysdata.net';
+    $dbName = 'sae-nmms_eureka';
+    $username = 'sae-nmms';
+    $password = 'NicolMonterdeMiquelSchardt';
+
+    try {
+        // Create a new PDO instance
+        $pdo = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+        
+        // Set PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // You can now use the $pdo object to perform database operations
+        
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+    return $pdo;
+}
+
+function planningPerUser($pdo, $user_id) {
+    $requete = $pdo-> prepare("SELECT ap.start, ap.duration, c.name
+                               FROM Appointment ap
+                               JOIN User u on ap.user_id = u.user_id
+                               JOIN Speaker s on ap.speaker_id = s.speaker_id
+                               JOIN Company c on s.company_id = c.company_id
+                               WHERE u.user_id = ?");
+    $requete->execute([$user_id]);
+    $planning = [];
+    $i = 0;
+    while ($ligne = $requete->fetch()) {
+        $planning[$i]['start'] = $ligne['start'];
+        $planning[$i]['company_name'] = $ligne['name'];
+        list($hours, $minutes, $seconds) = sscanf($ligne['duration'], '%d:%d:%d');
+        $interval = new DateInterval(sprintf('PT%dH%dM%dS', $hours, $minutes, $seconds));
+        $planning[$i]['end'] = (date_add(new DateTime($ligne['start']), $interval))->format('H:i:s');
+        $i++;
+    }
+    return $planning;
+}
+
+function unlistedCompanyPerUser($pdo, $user_id) {
+    $requete = $pdo-> prepare("SELECT c.name 
+                               FROM Company c
+                               JOIN WishList w on c.company_id = w.company_id
+                               WHERE c.excluded = 1
+                               AND w.user_id = ?");
+    $requete->execute([$user_id]);
+    return $requete;
+}
+
 ?>
--- pages/etudiant/phase2/emploiDuTemps.php
+++ pages/etudiant/phase2/emploiDuTemps.php
@@ -4,36 +4,55 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../bootstrap-4.6.2-dist/css/bootstrap.css">
-        <link rel="stylesheet" href="../fontawesome-free-6.2.1-web/css/all.css">
+        <link rel="stylesheet" href="../../../css/emploiDuTemps.css">
+        
+        <link rel="stylesheet" href="../../../lib/bootstrap-5.3.2-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="../../../lib/fontawesome-free-6.2.1-web/css/all.css">
+        
+        <script src="../../../lib/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
+        <link rel="stylesheet" href="../../../css/navbars.css">
         <title>pas dev</title>
     </head>
     <body>
+        <nav class="navbar navbar-expand sticky-top border bg-white">
+            <div class="container-fluid">
+                <div class="navbar-brand">
+                    <img src="../../../ressources/logo_black.png" alt="Logo Eureka" class="d-inline-block align-text-top">
+                    Eureka
+                </div>
+                <div class="navbar-right">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown p-2 d-none d-sm-block fond_inactif_haut">
+                            <a class="dropdown-toggle lien couleur_inactif_haut" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pseudo Utilisateur
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li> <a class="dropdown-item" href="../../deconnexion.php"> Se déconnecter </a> </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item d-sm-none">
+                            <a href="../../deconnexion.php">
+                                <img src="../../../ressources/icone_deconnexion.png" alt="Se déconnecter">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <?php
-            
-            // // Récupérer le nom de la page
-            // $currentPage = basename(__FILE__);
-
-
-            // // Afficher le nom de la page
-            // echo "<h1>Page: $currentPage</h1>";
-
-            // echo "<p>Cette page n'est pas développer.</p>";
-
-
-            // // Afficher les variables de session
-            // echo "<h2>Variables de session :</h2>";
-            // var_dump($_SESSION);
-
-            // // Afficher les variables POST
-            // echo "<h2>Variables POST :</h2>";
-            // var_dump($_POST);
-       
-            // // Afficher les variables GET       
-            // echo "<h2>Variables GET :</h2>";       
-            // var_dump($_GET);       
+            require('../../../fonctions/baseDeDonnees.php');
+            $pdo = connecteBD();
+            $planning = planningPerUser($pdo, 1);
+            echo "<h1>Planning</h1>";
+            foreach ($planning as $rdv) {
+                echo $rdv['start'] . ' ' . $rdv['company_name'] . ' ' . $rdv['end'] . '<br>';
+            }
+            echo '<h1>Entreprises non listées</h1>';
+            $unlistedCompany = unlistedCompanyPerUser($pdo, 1);
+            while ($ligne = $unlistedCompany->fetch()) {
+                echo $ligne['name'] . '<br>';
+            }
         ?> 
 
-        
     </body>
 </html>
