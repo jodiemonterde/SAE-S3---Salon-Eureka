a27495f2b627860b7cf713b599ae013ade6adf80
Benicol <benjamin.nicol@iut-rodez.fr>
2023-12-17 11:03:19 +0100

    creation des fichiers

---
+++ filtre.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="lib/bootstrap-4.6.2-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="lib/fontawesome-free-6.2.1-web/css/all.css">
+        <link rel="stylesheet" href="filtre.css">
+        <title>pas dev</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
