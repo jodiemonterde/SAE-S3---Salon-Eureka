40b5cc4974e3d7f851826667a18fa91696f0f2a8
Benicol <benjamin.nicol@iut-rodez.fr>
2023-12-19 11:43:26 +0100

    en cours

--- pages/etudiant/phase1/listeEntreprises.php
+++ pages/etudiant/phase1/listeEntreprises.php
@@ -14,26 +14,46 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../../../lib/bootstrap-5.3.2-dist/css/bootstrap.css">
         <link rel="stylesheet" href="../../../lib/fontawesome-free-6.5.1-web/css/all.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
-        <link rel="stylesheet" href="./listeEntreprise.css">
+        <link rel="stylesheet" href="../../../css/listeEntreprise.css">
+        <link rel="stylesheet" href="../../../css/navbars.css">
+        <script src="./lib/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
         <title> Eureka - Liste des entreprises </title>
     </head>
     <body>
-        <?php
-            // Récupérer le nom de la page
-            $currentPage = basename(__FILE__);
-        ?>
-
-        <div class="navbar navbar-default sticky-top d-none d-sm-block bg-secondary" role="navigation">
-            <div class="container">
-                <div class="navbar-brand">
-                    Logo Eureka
+        <!-- Navbar du haut -->
+        <nav class="navbar navbar-expand sticky-top border-bottom bg-white p-0">
+            <div class="container-fluid h-100">
+                <div class="navbar-brand d-flex align-items-center h-100">
+                    <img src="./ressources/logo_black.png" alt="Logo Eureka" class="logo me-2">
+                    Eureka
                 </div>
-                <div class="navbar-right">
-                    <input type="submit" class="btn btn-light" value="Logo Bouton Déconnexion">
+                <div class="navbar-right h-100">
+                    <ul class="navbar-nav d-flex h-100 align-items-center">
+                        <li class="nav-item nav-link p-0 d-none d-md-block h-100">
+                            <!-- Si sur la liste des entreprises, mettre en actif -->
+                            <a class="actif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des entreprises </a>
+                        </li>
+                        <li class="nav-item nav-link p-0 h-100 d-none d-md-block">
+                            <!-- Si sur la liste des rendez-vous, mettre en actif -->
+                            <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Mes rendez-vous </a>
+                        </li>
+                        <li class="nav-item dropdown p-0 h-100 d-none d-md-block">
+                            <a class="dropdown-toggle inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pseudo Utilisateur
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li> <a class="dropdown-item" href="#"> Se déconnecter </a> </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item d-md-none d-flex justify-content-end">
+                            <a href="#">
+                                <img src="./ressources/icone_deconnexion.png" alt="Se déconnecter">
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </div>
+        </nav>
 
         <div class="container">
             <div class="row d-flex align-items-center h-100">
