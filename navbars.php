3b44922483b9e0f74d5272aefef41449f0bcd3f8
UgoSchardt <ugo.schardt@iut-rodez.fr>
2023-12-19 11:27:19 +0100

    Réparation de problèmes sur les navbars

--- navbars.php
+++ navbars.php
@@ -6,41 +6,37 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./lib/bootstrap-5.3.2-dist/css/bootstrap.css">
         <link rel="stylesheet" href="./lib/fontawesome-free-6.2.1-web/css/all.css">
-        <link rel="stylesheet" href="./navbars.css">
+        <link rel="stylesheet" href="./css/navbars.css">
         <script src="./lib/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
         <title> Navbars </title>
     </head>
     <body>
         <!-- Navbar du haut -->
-        <nav class="navbar navbar-expand sticky-top border bg-white">
-            <div class="container-fluid">
-                <div class="navbar-brand">
-                    <img src="./ressources/logo_black.png" alt="Logo Eureka" class="d-inline-block align-text-top">
+        <nav class="navbar navbar-expand sticky-top border-bottom bg-white p-0">
+            <div class="container-fluid h-100">
+                <div class="navbar-brand d-flex align-items-center h-100">
+                    <img src="./ressources/logo_black.png" alt="Logo Eureka" class="logo me-2">
                     Eureka
                 </div>
-                <div class="navbar-right">
-                    <ul class="navbar-nav">
-                        <li class="nav-item d-none d-sm-block">
-                            <!-- Si sur la liste des entreprises, mettre en jaune -->
-                            <a class="actif_haut btn" role="button" href="#">
-                                Liste des entreprises
-                            </a>
+                <div class="navbar-right h-100">
+                    <ul class="navbar-nav d-flex h-100 align-items-center">
+                        <li class="nav-item nav-link p-0 d-none d-md-block h-100">
+                            <!-- Si sur la liste des entreprises, mettre en actif -->
+                            <a class="actif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des entreprises </a>
                         </li>
-                        <li class="nav-item d-none d-sm-block">
-                            <!-- Si sur la liste des rendez-vous, mettre en jaune -->
-                            <a class="inactif_haut btn" role="button" href="#">
-                                Mes rendez-vous
-                            </a>
+                        <li class="nav-item nav-link p-0 h-100 d-none d-md-block">
+                            <!-- Si sur la liste des rendez-vous, mettre en actif -->
+                            <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Mes rendez-vous </a>
                         </li>
-                        <li class="nav-item d-none d-sm-block dropdown">
-                            <a class="inactif_haut btn dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <li class="nav-item dropdown p-0 h-100 d-none d-md-block">
+                            <a class="dropdown-toggle inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Pseudo Utilisateur
                             </a>
                             <ul class="dropdown-menu" role="menu">
-                                <li> <a class="dropdown-item"> Se déconnecter </a> </li>
+                                <li> <a class="dropdown-item" href="#"> Se déconnecter </a> </li>
                             </ul>
                         </li>
-                        <li class="nav-item d-sm-none">
+                        <li class="nav-item d-md-none d-flex justify-content-end">
                             <a href="#">
                                 <img src="./ressources/icone_deconnexion.png" alt="Se déconnecter">
                             </a>
@@ -81,23 +77,23 @@
         </div>
 
         <!-- Navbar du bas -->
-        <nav class="navbar navbar-expand fixed-bottom d-sm-none border bg-white">
+        <nav class="navbar navbar-expand fixed-bottom d-md-none border bg-white">
             <div class="container-fluid">
                 <ul class="navbar-nav w-100 justify-content-evenly">
-                    <li class="nav-item actif_bas_texte">
-                        <!-- Si sur la liste des entreprises, mettre l'icone blanche et le fond en jaune -->
+                    <!-- Si sur la liste des entreprises, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center actif_bas_texte">
+                        <!-- Si sur la liste des entreprises, mettre l'icone en actif -->
                         <a class="d-flex justify-content-center actif_bas_icone" href="#">
                             <img src="./ressources/entreprise_white.png" alt="Liste des entreprises">
                         </a>
-                        <!-- Si sur la liste des entreprises, mettre en jaune -->
                         Entreprises
                     </li>
-                    <li class="nav-item inactif_bas">
-                        <!-- Si sur la liste des rendez-vous, mettre l'icone blanche et le fond en jaune -->
+                    <!-- Si sur la liste des rendez-vous, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center inactif_bas">
+                        <!-- Si sur la liste des rendez-vous, mettre l'icône en actif -->
                         <a class="d-flex justify-content-center" href="#">
                             <img src="./ressources/rendez-vous_black.png" alt="Mes rendez-vous">
                         </a>
-                        <!-- Si sur la liste des rendez-vous, mettre en jaune -->
                         Rendez-vous
                     </li>
                 </ul>
