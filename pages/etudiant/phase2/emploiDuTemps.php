e0051cee181829731cb23ba1bc3fbf57e0ef4f5a
Benicol <benjamin.nicol@iut-rodez.fr>
2023-12-19 18:57:38 +0100

    fin, près a merge

--- pages/etudiant/phase2/emploiDuTemps.php
+++ pages/etudiant/phase2/emploiDuTemps.php
@@ -13,28 +13,30 @@
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="../../../lib/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
+        <script src="../../../js/downloadpage.js"></script>
         <link rel="stylesheet" href="../../../css/navbars.css">
         <title>Planning</title>
     </head>
     <body>
-        <nav class="navbar navbar-expand sticky-top border bg-white">
-            <div class="container-fluid">
-                <div class="navbar-brand">
-                    <img src="../../../ressources/logo_black.png" alt="Logo Eureka" class="d-inline-block align-text-top">
+    <nav class="navbar navbar-expand sticky-top border-bottom bg-white p-0">
+            <div class="container-fluid h-100">
+                <div class="navbar-brand d-flex align-items-center h-100">
+                    <img src="../../../ressources/logo_black.png" alt="Logo Eureka" class="logo me-2">
                     Eureka
                 </div>
-                <div class="navbar-right">
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown p-2 d-none d-sm-block fond_inactif_haut">
-                            <a class="dropdown-toggle lien couleur_inactif_haut" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <div class="navbar-right h-100">
+                    <ul class="navbar-nav d-flex h-100 align-items-center">
+                        
+                        <li class="nav-item dropdown p-0 h-100 d-none d-md-block">
+                            <a class="dropdown-toggle inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Pseudo Utilisateur
                             </a>
                             <ul class="dropdown-menu" role="menu">
-                                <li> <a class="dropdown-item" href="../../deconnexion.php"> Se déconnecter </a> </li>
+                                <li> <a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deconnexion"> Se déconnecter </a> </li>
                             </ul>
                         </li>
-                        <li class="nav-item d-sm-none">
-                            <a href="../../deconnexion.php">
+                        <li class="nav-item d-md-none d-flex justify-content-end">
+                            <a data-bs-toggle="modal" data-bs-target="#deconnexion">
                                 <img src="../../../ressources/icone_deconnexion.png" alt="Se déconnecter">
                             </a>
                         </li>
@@ -85,9 +87,36 @@
                 </div>
             </div>
         </div>
-        <?php if (isset($_POST['telecharger'])) {?>
-            
-        <?php } ?>
+        <div class="modal fade " id="deconnexion" tabindex="-1" aria-labelledby="Sedeconnecter" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-fullscreen-sm-down">
+                <div class="modal-content">
+                    <div class="modal-header deco">
+                        <button type="button" class="blanc" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-arrow-left"></i></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class = "row">
+                                <div class="col-12">
+                                    <h1 class="text-center" id="Sedeconnecter">DÉCONNEXION</h1>
+                                </div>
+                            </div>
+                            <div class = "row">
+                                <div class="col-12">
+                                    <P class="text-center">Êtes-vous sûr(e) de vouloir vous déconnecter ?</P>
+                                </div>
+                            </div>
+                            <div class = "row">
+                                <div class="col-6 d-flex justify-content-evenly">
+                                    <button type="button" data-bs-dismiss="modal" class="bouton">Retour</button>
+                                </div>
+                                <div class="col-6 d-flex justify-content-evenly">
+                                    <a href="../../../fonctions/deconnecter.php"><button type="button" class="bouton" >Se déconnecter </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
-    <script src="../../../js/downloadpage.js"></script>
 </html>
