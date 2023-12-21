2659e6ee0e16527429e0c03051f8adb4d8e4b1c6
NoahMiquel <noah.miquel@iut-rodez.fr>
2023-12-21 10:16:52 +0100

    navbar ajouté

--- pages/administrateur/forum/menu.php
+++ pages/administrateur/forum/menu.php
@@ -19,7 +19,48 @@
         <title>informations eureka</title>
     </head>
     <body>
-        <h1 class="text-center"> navbar </h1>
+             <!-- Navbar du haut -->
+             <nav class="navbar navbar-expand sticky-top border-bottom bg-white p-0">
+            <div class="container-fluid h-100">
+                <div class="navbar-brand d-flex align-items-center h-100">
+                    <img src="./ressources/logo_black.svg" alt="Logo Eureka" class="logo me-2">
+                    Eureka
+                </div>
+                <div class="navbar-right h-100">
+                    <ul class="navbar-nav d-flex h-100 align-items-center">
+                        <li class="nav-item nav-link p-0 d-none d-md-block h-100">
+                            <!-- Si sur la liste des entreprises, mettre en actif et lien_inactif-->
+                            <a class="actif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des entreprises </a>
+                        </li>
+                        <li class="nav-item nav-link p-0 h-100 d-none d-md-block">
+                            <!-- Si sur la liste des étudiants, mettre en actif et lien_inactif -->
+                            <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des étudiants </a>
+                        </li>
+                        <li class="nav-item nav-link p-0 h-100 d-none d-md-block">
+                            <!-- Si sur la liste des gestionnaires, mettre en actif et lien_inactif -->
+                            <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des gestionnaires </a>
+                        </li>
+                        <li class="nav-item nav-link p-0 h-100 d-none d-md-block">
+                            <!-- Si sur les paramètres du forum, mettre en actif et lien_inactif -->
+                            <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Paramètres du forum </a>
+                        </li>
+                        <li class="nav-item dropdown p-0 h-100 d-none d-md-block">
+                            <a class="dropdown-toggle inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pseudo Utilisateur
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li> <a class="dropdown-item" href="#"> Se déconnecter </a> </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item d-md-none d-flex justify-content-end">
+                            <a href="#">
+                                <img src="./ressources/icone_deconnexion.svg" alt="Se déconnecter" class="logo">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <div class="container">
             <div class="row mx-1">
                 <div class="col-md-4"></div>
@@ -75,6 +116,49 @@
                 <div class="col-md-4"></div>
             </div>
         </div>
+                <!-- Navbar du bas -->
+        <nav class="navbar navbar-expand fixed-bottom d-md-none border bg-white">
+            <div class="container-fluid">
+                <ul class="navbar-nav w-100 justify-content-evenly">
+                    <!-- Si sur la liste des entreprises, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center actif_bas_texte">
+                        <!-- Si sur la liste des entreprises, mettre l'icone en actif et lien_inactif -->
+                        <a class="d-flex justify-content-center actif_bas_icone" href="#">
+                            <!-- Si sur la liste des entreprises, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                            <img src="./ressources/icone_entreprise_white.svg" alt="Liste des entreprises" class="icone">
+                        </a>
+                        Entreprises
+                    </li>
+                    <!-- Si sur la liste des étudiants, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center inactif_bas">
+                        <!-- Si sur la liste des étudiants, mettre l'icône en actif et lien_inactif -->
+                        <a class="d-flex justify-content-center" href="#">
+                            <!-- Si sur la liste des étudiants, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                            <img src="./ressources/icone_etudiant_black.svg" alt="Liste des étudiants" class="icone">
+                        </a>
+                        Etudiants
+                    </li>
+                    <!-- Si sur la liste des gestionnaires, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center inactif_bas">
+                        <!-- Si sur la liste des gestionnaires, mettre l'icône en actif et lien_inactif -->
+                        <a class="d-flex justify-content-center" href="#">
+                            <!-- Si sur la liste des gestionnaires, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                            <img src="./ressources/icone_gestionnaire_black.svg" alt="Liste des gestionnaires" class="icone">
+                        </a>
+                        Gestionnaires
+                    </li>
+                    <!-- Si sur les paramètres du forum, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center inactif_bas">
+                        <!-- Si sur les paramètres du forum, mettre l'icône en actif et lien_inactif -->
+                        <a class="d-flex justify-content-center" href="#">
+                            <!-- Si sur les paramètres du forum, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                            <img src="./ressources/icone_forum_black.svg" alt="Paramètres du forum" class="icone">
+                        </a>
+                        Forum
+                    </li>
+                </ul>
+            </div>
+        </nav>
         
         <!-- Modal de confirmation pour la réinitialisation des données -->
         <div class="modal fade " id="réinitialisation" tabindex="-1" aria-labelledby="réinitialiser" aria-hidden="true">
