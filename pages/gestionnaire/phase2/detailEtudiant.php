f093c217149a12d4e935f014415fc4ed84ce6ff3
NoahMiquel <noah.miquel@iut-rodez.fr>
2023-12-21 17:20:39 +0100

    modif

--- pages/gestionnaire/phase2/detailEtudiant.php
+++ pages/gestionnaire/phase2/detailEtudiant.php
@@ -31,6 +31,7 @@
     <link rel="stylesheet" href="../../../../outils/bootstrap-5.3.2-dist/css/bootstrap.css">
     <link rel="stylesheet" href="../../../../outils/fontawesome-free-6.5.1-web/css/all.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../../../outils/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <link rel="stylesheet" href="listeEtudiant.css">
     <link rel="stylesheet" href="../../../css/navbars.css">
@@ -38,34 +39,34 @@
     <title>Eureka - Liste des entreprises</title>
 </head>
 <body>
-         <!-- Navbar du haut -->
-    <nav class="navbar navbar-expand sticky-top border bg-white">
+   <!-- Navbar du haut -->
+   <nav class="navbar navbar-expand sticky-top border-bottom bg-white p-0">
         <div class="container-fluid h-100">
             <div class="navbar-brand d-flex align-items-center h-100">
-                <img src="../../../ressources/logo_black.png" alt="Logo Eureka" class="d-inline-block align-text-top me-2">
+                <img src="../../../ressources/logo_black.svg" alt="Logo Eureka" class="logo me-2">
                 Eureka
             </div>
             <div class="navbar-right h-100">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav d-flex h-100 align-items-center">
                     <li class="nav-item nav-link p-0 d-none d-md-block h-100">
-                        <!-- Si sur la liste des entreprises, mettre en jaune -->
-                        <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="listeEntreprises.php"> Liste des entreprises </a>
+                        <!-- Si sur la liste des entreprises, mettre en actif et lien_inactif-->
+                        <a class="actif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des entreprises </a>
                     </li>
                     <li class="nav-item nav-link p-0 h-100 d-none d-md-block">
-                        <!-- Si sur la liste des rendez-vous, mettre en jaune -->
-                        <a class="actif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Mes rendez-vous </a>
+                        <!-- Si sur la liste des étudiants, mettre en actif et lien_inactif -->
+                        <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des étudiants </a>
                     </li>
                     <li class="nav-item dropdown p-0 h-100 d-none d-md-block">
                         <a class="dropdown-toggle inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Pseudo Utilisateur
                         </a>
                         <ul class="dropdown-menu" role="menu">
-                            <li> <a class="dropdown-item" href="../../deconnexion.php"> Se déconnecter </a> </li>
+                            <li> <a class="dropdown-item" href="#"> Se déconnecter </a> </li>
                         </ul>
                     </li>
                     <li class="nav-item d-md-none d-flex justify-content-end">
-                        <a href="../../deconnexion.php">
-                            <img src="../../../ressources/icone_deconnexion.png" alt="Se déconnecter">
+                        <a href="#">
+                            <img src="../../../ressources/icone_deconnexion.svg" alt="Se déconnecter" class="logo">
                         </a>
                     </li>
                 </ul>
@@ -170,24 +171,26 @@
     <nav class="navbar navbar-expand fixed-bottom d-md-none border bg-white">
         <div class="container-fluid">
             <ul class="navbar-nav w-100 justify-content-evenly">
-                <li class="nav-item actif_bas_texte">
-                    <!-- Si sur la liste des entreprises, mettre l'icone blanche et le fond en jaune -->
+                <!-- Si sur la liste des entreprises, mettre le texte en actif -->
+                <li class="nav-item d-flex flex-column text-center actif_bas_texte">
+                    <!-- Si sur la liste des entreprises, mettre l'icône en actif et lien_inactif -->
                     <a class="d-flex justify-content-center actif_bas_icone" href="#">
-                        <img src="../../../ressources/entreprise_white.png" alt="Liste des entreprises">
+                        <!-- Si sur la liste des entreprises, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                        <img src="../../../ressources/icone_entreprise_white.svg" alt="Liste des entreprises" class="icone">
                     </a>
-                    <!-- Si sur la liste des entreprises, mettre en jaune -->
                     Entreprises
                 </li>
-                <li class="nav-item inactif_bas">
-                    <!-- Si sur la liste des rendez-vous, mettre l'icone blanche et le fond en jaune -->
+                <!-- Si sur la liste des étudiants, mettre le texte en actif -->
+                <li class="nav-item d-flex flex-column text-center inactif_bas">
+                    <!-- Si sur la liste des étudiants, mettre l'icône en actif et lien_inactif -->
                     <a class="d-flex justify-content-center" href="#">
-                        <img src="../../../ressources/rendez-vous_black.png" alt="Mes rendez-vous">
+                        <!-- Si sur la liste des étudiants, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                        <img src="../../../ressources/icone_etudiant_black.svg" alt="Liste des étudiants" class="icone">
                     </a>
-                    <!-- Si sur la liste des rendez-vous, mettre en jaune -->
-                    Rendez-vous
+                    Etudiants
                 </li>
             </ul>
         </div>
-    </nav>   
+    </nav>
 </body>
 </html>
