a1845daf1c8b319591eb740d005debfa80e0a898
NoahMiquel <noah.miquel@iut-rodez.fr>
2023-12-21 13:33:15 +0100

    navbar

--- pages/gestionnaire/phase2/detailEtudiant.php
+++ pages/gestionnaire/phase2/detailEtudiant.php
@@ -9,29 +9,65 @@
         <title>pas dev</title>
     </head>
     <body>
-        <?php
-            
-            // Récupérer le nom de la page
-            $currentPage = basename(__FILE__);
+         <!-- Navbar du haut -->
+         <nav class="navbar navbar-expand sticky-top border-bottom bg-white p-0">
+            <div class="container-fluid h-100">
+                <div class="navbar-brand d-flex align-items-center h-100">
+                    <img src="./ressources/logo_black.svg" alt="Logo Eureka" class="logo me-2">
+                    Eureka
+                </div>
+                <div class="navbar-right h-100">
+                    <ul class="navbar-nav d-flex h-100 align-items-center">
+                        <li class="nav-item nav-link p-0 d-none d-md-block h-100">
+                            <!-- Si sur la liste des entreprises, mettre en actif et lien_inactif-->
+                            <a class="actif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des entreprises </a>
+                        </li>
+                        <li class="nav-item nav-link p-0 h-100 d-none d-md-block">
+                            <!-- Si sur la liste des étudiants, mettre en actif et lien_inactif -->
+                            <a class="inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" href="#"> Liste des étudiants </a>
+                        </li>
+                        <li class="nav-item dropdown p-0 h-100 d-none d-md-block">
+                            <a class="dropdown-toggle inactif_haut d-flex align-items-center h-100 px-2 justify-content-center text-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Pseudo Utilisateur
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li> <a class="dropdown-item" href="#"> Se déconnecter </a> </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item d-md-none d-flex justify-content-end">
+                            <a href="#">
+                                <img src="./ressources/icone_deconnexion.svg" alt="Se déconnecter" class="logo">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-
-            // Afficher le nom de la page
-            echo "<h1>Page: $currentPage</h1>";
-
-            echo "<p>Cette page n'est pas développer.</p>";
-
-
-            // Afficher les variables de session
-            echo "<h2>Variables de session :</h2>";
-            var_dump($_SESSION);
-
-            // Afficher les variables POST
-            echo "<h2>Variables POST :</h2>";
-            var_dump($_POST);
-
-            // Afficher les variables GET
-            echo "<h2>Variables GET :</h2>";
-            var_dump($_GET);
-        ?>
+        <!-- Navbar du bas -->
+        <nav class="navbar navbar-expand fixed-bottom d-md-none border bg-white">
+            <div class="container-fluid">
+                <ul class="navbar-nav w-100 justify-content-evenly">
+                    <!-- Si sur la liste des entreprises, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center actif_bas_texte">
+                        <!-- Si sur la liste des entreprises, mettre l'icône en actif et lien_inactif -->
+                        <a class="d-flex justify-content-center actif_bas_icone" href="#">
+                            <!-- Si sur la liste des entreprises, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                            <img src="./ressources/icone_entreprise_white.svg" alt="Liste des entreprises" class="icone">
+                        </a>
+                        Entreprises
+                    </li>
+                    <!-- Si sur la liste des étudiants, mettre le texte en actif -->
+                    <li class="nav-item d-flex flex-column text-center inactif_bas">
+                        <!-- Si sur la liste des étudiants, mettre l'icône en actif et lien_inactif -->
+                        <a class="d-flex justify-content-center" href="#">
+                            <!-- Si sur la liste des étudiants, mettre l'icône blanche, sinon mettre l'icône en noir -->
+                            <img src="./ressources/icone_etudiant_black.svg" alt="Liste des étudiants" class="icone">
+                        </a>
+                        Etudiants
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </body>
 </html>
