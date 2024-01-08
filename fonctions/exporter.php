e4236440c345b46a2eaf1a593a9e3b5d8d774fc3
NoahMiquel <noah.miquel@iut-rodez.fr>
2024-01-08 10:17:18 +0100

    correction lien et mise en forme pdf

--- fonctions/exporter.php
+++ fonctions/exporter.php
@@ -1,19 +1,21 @@
 <?php
-    require("C:/sae/UWamp-dev-cote-serv_2023_Vide_PHP_8_OK/www/GitHub/outils/fpdf/fpdf.php");
+    require('lib/fpdf.php');
     require_once("baseDeDonnees.php");
     
     function exportEntreprise($company_id,$pdo){
         $nom = 'planningEntreprise.pdf';
         $pdfEntreprise = new FPDF();
         $pdfEntreprise->AddPage();
-        $pdfEntreprise->SetFont('Arial','',11);
+        $pdfEntreprise->SetDrawColor(217,217,217);
         $planningEntreprise= getStudentsAppointmentsPerCompany($pdo, $company_id, 0);
         while($row = $planningEntreprise->fetch()){
             $pdfEntreprise->SetFont('Arial','B',11);
             $pdfEntreprise->Cell(0, 10, $row["start"].'-'.$row["duration"], 'LTR', 1, 'C');
             $pdfEntreprise->SetFont('Arial','',11);
+            $pdfEntreprise->SetTextColor(255 , 168, 0);
             $pdfEntreprise->Cell(0, 10,$row["name"] , 'LR', 1, 'C');
             $pdfEntreprise->Cell(0, 10,$row["username"] , 'LBR', 1, 'C');
+            $pdfEntreprise->SetTextColor(0 , 0, 0);
             $pdfEntreprise->Ln();
         }
         $pdfEntreprise->Output($nom,'D');
