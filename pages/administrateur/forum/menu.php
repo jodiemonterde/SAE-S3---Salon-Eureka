6cd7b896b82549537d539cec51ecb5ff21585e3c
Benicol <benjamin.nicol@iut-rodez.fr>
2024-01-09 21:51:49 +0100

    ptit fix

--- pages/administrateur/forum/menu.php
+++ pages/administrateur/forum/menu.php
@@ -30,8 +30,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="../../../css/all.css">
         <link rel="stylesheet" href="../../../css/navbars.css">
-        <script src="../../../../outils/bootstrap-5.3.2-dist/js/bootstrap.js"></script>
-        <script src="../../../../outils/bootstrap-5.3.2-dist/js/bootstrap.bundle.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <link rel="stylesheet" href="../../../css/forum.css">
         <title>informations eureka</title>
     </head>
@@ -204,7 +204,7 @@
                             </div>
                             <div class = "row">
                                 <div class="col-12">
-                                    <P class="text-center">Êtes-vous sûr(e) de vouloir réinitialiser les données ? <span class="erreur">Cela supprimera TOUS les données du site SAUF les administrateurs</span></P>
+                                    <P class="text-center">Êtes-vous sûr(e) de vouloir réinitialiser les données ? <span class="erreur">Cela supprimera TOUTES les données du site SAUF les administrateurs</span></P>
                                     <P class="text-center">Taper "Je reinitialise les données du site" pour confirmer</P>
                                 </div>
                             </div>
