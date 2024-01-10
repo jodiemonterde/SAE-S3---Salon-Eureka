50c7891ce943f7979cb475e2d68207994c095555
NoahMiquel <noah.miquel@iut-rodez.fr>
2024-01-10 15:27:05 +0100

    mise a jour format pdf lors de l'exportation des planning

--- fonctions/fonctionsImportationExportation.php
+++ fonctions/fonctionsImportationExportation.php
@@ -11,18 +11,22 @@
         $pdfEntreprise->Cell(0, 10, conversion($nomEntreprise), 0, 1, 'C');
         $pdfEntreprise->SetDrawColor(217,217,217);
         $listeSpeaker = getSpeakersPerCompany($pdo, $company_id);
+        $i = 0; 
         while($row1 = $listeSpeaker->fetch()){
+            $i = $i+ 1;
             $rendezVous = getAppointmentPerSpeaker($pdo,$row1["speaker_id"]);
             $pdfEntreprise->SetFont('Arial','B',11);
             $pdfEntreprise->Cell(0, 10, conversion($row1["name"]), 0, 1, 'C');
             while($row2 = $rendezVous->fetch()){
-                $pdfEntreprise->Cell(0, 10, $row2["start"].'-'.$row2["end"], 'LTR', 1, 'C');
+                $pdfEntreprise->Cell(95, 10, $row2["start"].'-'.$row2["end"], 'LTBR', 0, 'C');
                 $pdfEntreprise->SetFont('Arial','',11);
-                $pdfEntreprise->SetTextColor(255 , 168, 0);
-                $pdfEntreprise->Cell(0, 10, conversion($row2["username"]) , 'LBR', 1, 'C');
+                $pdfEntreprise->SetTextColor(139, 45, 45);
+                $pdfEntreprise->Cell(95, 10, conversion($row2["username"]) , 'LTBR', 1, 'C');
                 $pdfEntreprise->SetTextColor(0 , 0, 0);
                 $pdfEntreprise->SetFont('Arial','B',11);
-                $pdfEntreprise->Ln();
+            }
+            if($i < $listeSpeaker->rowCount()){
+                $pdfEntreprise->AddPage();
             }
         }
         $pdfEntreprise->Output($nom,'D');
@@ -44,24 +48,24 @@
         $planning = planningPerUser($pdo, $student_id);
         foreach ($planning as $rdv) {
             $pdfEtudiant->SetFont('Arial','B',11);
-            $pdfEtudiant->Cell(0, 10, $rdv['start'].'-'.$rdv['end'], 'LTR', 1, 'C');
+            $pdfEtudiant->Cell(95, 10, $rdv['start'].'-'.$rdv['end'], 'LTBR', 0, 'C');
             $pdfEtudiant->SetFont('Arial','',11);
-            $pdfEtudiant->SetTextColor(255 , 168, 0);
-            $pdfEtudiant->Cell(0, 10,conversion($rdv['company_name']) , 'LBR', 1, 'C');
+            $pdfEtudiant->SetTextColor(139, 45, 45);
+            $pdfEtudiant->Cell(95, 10,conversion($rdv['company_name']) , 'LTBR', 0, 'C');
             $pdfEtudiant->SetTextColor(0 , 0, 0);
             $pdfEtudiant->Ln();
         }
         $unlisted = unlistedCompanyPerUser($pdo, $student_id);
         if ($unlisted->rowCount() > 0) {
             $pdfEtudiant->SetFont('Arial','B',20);
+            $pdfEtudiant->Ln();
             $pdfEtudiant->Cell(0, 10, conversion("Entreprises hors planning à aller voir"),0, 1, 'C');
             $pdfEtudiant->SetFont('Arial','',11);
-            $pdfEtudiant->SetTextColor(255 , 168, 0);
+            $pdfEtudiant->SetTextColor(139, 45, 45);
             $pdfEtudiant->Ln();
 
             while($row = $unlisted->fetch()){
-                $pdfEtudiant->Cell(0, 10,conversion($row["name"]) , 'LTBR', 1, 'C');
-                $pdfEtudiant->Ln();
+                $pdfEtudiant->Cell(0, 10,conversion($row["name"]) , 'LTBR', 0, 'C');
             }
         }
         $pdfEtudiant->Output($nom,'D');
@@ -78,9 +82,9 @@
         $listeEtudiant = studentByUnlistedCompany($pdo,$company_id);
         while($row = $listeEtudiant->fetch()){
             $pdfExclu->SetFont('Arial','',11);
-            $pdfExclu->SetTextColor(255 , 168, 0);
-            $pdfExclu->Cell(0, 10,conversion($row["username"]) , 'LTR', 1, 'C');
-            $pdfExclu->Cell(0, 10,$row["name"] , 'LBR', 1, 'C');
+            $pdfExclu->SetTextColor(139, 45, 45);
+            $pdfExclu->Cell(95, 10,conversion($row["username"]) , 'LBTR', 0, 'C');
+            $pdfExclu->Cell(95, 10,conversion($row["name"]) , 'LTBR', 0, 'C');
             $pdfExclu->Ln();
         }
         $pdfExclu->Output($nom,'D');
@@ -96,20 +100,26 @@
             $pdfEntreprise->AddPage();
             $pdfEntreprise->SetFont('Arial','B',20);
             $pdfEntreprise->Cell(0, 10, conversion($ligne["name"]), 0, 1, 'C');
+            $i = 0;
             while($row1 = $listeSpeaker->fetch()){
+                $i =$i+1;
                 $rendezVous = getAppointmentPerSpeaker($pdo,$row1["speaker_id"]);
                 $pdfEntreprise->SetFont('Arial','B',11);
                 $pdfEntreprise->Cell(0, 10, conversion($row1["name"]), 0, 1, 'C');
                 while($row2 = $rendezVous->fetch()){
-                    $pdfEntreprise->Cell(0, 10, $row2["start"].'-'.$row2["end"], 'LTR', 1, 'C');
+                    $pdfEntreprise->Cell(95, 10, $row2["start"].'-'.$row2["end"], 'LTBR', 0, 'C');
                     $pdfEntreprise->SetFont('Arial','',11);
-                    $pdfEntreprise->SetTextColor(255 , 168, 0);
-                    $pdfEntreprise->Cell(0, 10, conversion($row2["username"]) , 'LBR', 1, 'C');
+                    $pdfEntreprise->SetTextColor(139, 45, 45);
+                    $pdfEntreprise->Cell(95, 10, conversion($row2["username"]) , 'LBTR', 0, 'C');
                     $pdfEntreprise->SetTextColor(0 , 0, 0);
                     $pdfEntreprise->SetFont('Arial','B',11);
                     $pdfEntreprise->Ln();
                 }
+                if($i < $listeSpeaker->rowCount()){
+                    $pdfEntreprise->AddPage();
+                }
             }
+
         }
         $pdfEntreprise->Output($nom,'D');
     }
@@ -127,27 +137,29 @@
                 $pdfEtudiant->Cell(0, 10,conversion($ligne["username"]),0, 1, 'C');
                 foreach ($planning as $rdv) {
                     $pdfEtudiant->SetFont('Arial','B',11);
-                    $pdfEtudiant->Cell(0, 10, $rdv['start'].'-'.$rdv['end'], 'LTR', 1, 'C');
-                    $pdfEtudiant->SetFont('Arial','',11);
-                    $pdfEtudiant->SetTextColor(255 , 168, 0);
-                    $pdfEtudiant->Cell(0, 10,conversion($rdv['company_name']) , 'LBR', 1, 'C');
                     $pdfEtudiant->SetTextColor(0 , 0, 0);
+                    $pdfEtudiant->Cell(95, 10, $rdv['start'].'-'.$rdv['end'], 'LBTR', 0, 'C');
+                    $pdfEtudiant->SetFont('Arial','',11);
+                    $pdfEtudiant->SetTextColor(139, 45, 45);
+                    $pdfEtudiant->Cell(95, 10,conversion($rdv['company_name']) , 'LTBR', 0, 'C');
                     $pdfEtudiant->Ln();
                 }
             }
             $unlisted = unlistedCompanyPerUser($pdo, $ligne["user_id"]);
             if ($unlisted->rowCount() > 0) {
+                $pdfEtudiant->Ln();
                 $pdfEtudiant->SetFont('Arial','B',20);
+                $pdfEtudiant->SetTextColor(0, 0, 0);
                 $pdfEtudiant->Cell(0, 10, conversion("Entreprises hors planning à aller voir"),0, 1, 'C');
                 $pdfEtudiant->SetFont('Arial','',11);
-                $pdfEtudiant->SetTextColor(255 , 168, 0);
+                $pdfEtudiant->SetTextColor(139, 45, 45);
                 $pdfEtudiant->Ln();
 
                 while($row = $unlisted->fetch()){
-                    $pdfEtudiant->Cell(0, 10,conversion($row["name"]) , 'LTBR', 1, 'C');
-                    $pdfEtudiant->Ln();
+                    $pdfEtudiant->Cell(0, 10,conversion($row["name"]) , 'LTBR', 0, 'C');
                 }
             }
+            $pdfEtudiant->SetTextColor(0 , 0, 0);
         }
         $pdfEtudiant->Output($nom,'D');
     }
@@ -165,9 +177,9 @@
             $listeEtudiant = studentByUnlistedCompany($pdo,$ligne["company_id"]);
             while($row = $listeEtudiant->fetch()){
                 $pdfExclu->SetFont('Arial','',11);
-                $pdfExclu->SetTextColor(255 , 168, 0);
-                $pdfExclu->Cell(0, 10,conversion($row["username"]) , 'LTR', 1, 'C');
-                $pdfExclu->Cell(0, 10,conversion($row["name"]) , 'LBR', 1, 'C');
+                $pdfExclu->SetTextColor(139, 45, 45);
+                $pdfExclu->Cell(95, 10,conversion($row["username"]) , 'LTBR', 0, 'C');
+                $pdfExclu->Cell(95, 10,conversion($row["name"]) , 'LTBR', 0, 'C');
                 $pdfExclu->Ln();
             }
         }
