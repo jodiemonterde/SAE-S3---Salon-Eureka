6511f1286c237adf064d1c7dca0c6fb3a596761d
Benicol <benjamin.nicol@iut-rodez.fr>
2024-01-11 18:55:55 +0100

    fin du merge

--- pages/administrateur/listeEtudiants.php
+++ pages/administrateur/listeEtudiants.php
@@ -184,7 +184,7 @@
             </div>
             <form action="listeEtudiants.php" method="post" class="col-12 col-md-6 my-2">
                 <div class="row">
-                    <div class="col-7 p-0">
+                    <div class="col-12 col-md-7 p-0">
                         <input type="search" name="recherche" value="<?php echo $_SESSION['recherche']; ?>" placeholder=" &#xf002 Rechercher un etudiant" class="zoneText"/>    
                     </div>
                     <div class="col-5 d-none d-md-block">
--- pages/etudiant/phase1/listeEntreprises.php
+++ pages/etudiant/phase1/listeEntreprises.php
@@ -104,7 +104,7 @@
                 </div>
                 <form action="listeEntreprises.php" method="post" class="col-12 col-md-6 my-2">
                     <div class="row">
-                        <div class="col-7 p-0">
+                        <div class="col-12 col-md-7 p-0">
                             <input type="search" name="recherche" value="<?php echo $_SESSION['recherche']; ?>" placeholder=" &#xf002 Rechercher une entreprise" class="entreeUtilisateur"/>    
                         </div>
                         <div class="col-5 d-none d-md-block">
